3cf9602692dc5e7e733dce856c9845b50d8b4115
gofish2020 <yu_shaona2020@163.com>
2022-02-14 18:58:51 +0800

    Update Stream.h

--- include/Stream.h
+++ include/Stream.h
@@ -8,7 +8,7 @@ class AFX_EXT_CLASS Stream
 public:
 	Stream();
 	virtual ~Stream();
-	virtual size_t Write(_In_ void* src, size_t count) = 0;
+	virtual size_t Write(_In_ void* src, size_t count) = 0; // string   wstring  
 	virtual size_t Read(_Out_ void* dest, size_t count)= 0;
 	virtual size_t GetSize() = 0;
 	virtual void SetCursor(size_t Pos) = 0;
