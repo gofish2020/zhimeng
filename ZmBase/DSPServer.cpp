0c4be6f0c6bb75280c7e185327a0d32fa3afcd0b
gofish2020 <yu_shaona2020@163.com>
2022-02-25 18:55:40 +0800

    1111

--- ZmBase/DSPServer.cpp
+++ ZmBase/DSPServer.cpp
@@ -5,69 +5,165 @@
 class HandleThread : public Thread
 {
 public:
-	HandleThread(SelectSocket *clientSocket) { c_clientSocket = clientSocket;  };
-	~HandleThread() {};
+	HandleThread(SelectSocket *clientSocket, LNetServerNotifyEvent *event);
+	~HandleThread();
 
-	void OnExecute() {};
+	void OnExecute();
 	UnicodeString ClassName() { return L"HandleThread"; }
 
 private:
 	SelectSocket *c_clientSocket;
+	LNetServerNotifyEvent *c_event;
 };
 
-
-
-
-///////////////////////////////////////////////
-// AcceptThread 监听线程 
-class AcceptThread : public Thread
+HandleThread::HandleThread(SelectSocket *clientSocket, LNetServerNotifyEvent *event)
 {
-public:
-	AcceptThread(SelectSocket *socket);
-	~AcceptThread();
-
-	void OnExecute();
-	UnicodeString ClassName() { return L"AcceptThread"; }
-
-private:
-	std::vector<Thread*>threads;
-	SelectSocket * c_socket;
-};
-
-AcceptThread::AcceptThread(SelectSocket *socket)
-{
-	c_socket = socket;
+	c_clientSocket = clientSocket;
+	c_event = event;
+	if (c_event != nullptr)
+	{
+		c_event->OnConnect(c_clientSocket);
+	}
 }
 
-void AcceptThread::OnExecute()
+HandleThread::~HandleThread()
+{
+	if (c_event != nullptr)
+	{
+		c_event->OnDisconnect(c_clientSocket);
+	}
+	if (c_clientSocket != nullptr)
+		delete c_clientSocket;
+	c_clientSocket = nullptr;
+}
+
+void HandleThread::OnExecute()
 {
 	while (!IsTerminate())
 	{
 		try
 		{
-			SelectSocket *clientSocket = c_socket->Accept(); //监听客户端的连接
-			if (clientSocket != nullptr)
+			if (c_event != nullptr && c_clientSocket != nullptr)
 			{
-				//接收或发送，数据的线程
-				Thread *temp = new HandleThread(clientSocket);
-				temp->Resume();
-				threads.push_back(temp);
+				c_event->OnExecute(c_clientSocket);
 			}
 		}
-		catch (const std::exception&e)
+		catch (const std::exception&)
 		{
-
+			Terminate();
 		}
 		catch (...)
 		{
-
+			Terminate();
 		}
 	}
 }
 
-AcceptThread::~AcceptThread()
+///////////////////////////////////////////////
+// ListenThread 监听线程 
+///////////////////////////////////////////////
+class ListenThread : public Thread, public ThreadNotifition
 {
+public:
+	ListenThread(SockSetting &setting,LNetServerNotifyEvent *event);
+	~ListenThread();
 
+	void OnExecute();
+	UnicodeString ClassName() { return L"ListenThread"; }
+	virtual void OnTerminate(Thread *thread) ;
+private:
+	CriticalSection c_Section;
+	std::vector<Thread*>childThreads;
+	SockSetting  c_setting;
+	SelectSocket *c_socket;
+	bool IsAccepting;
+	LNetServerNotifyEvent *c_event;
+};
+
+ListenThread::ListenThread(SockSetting &setting, LNetServerNotifyEvent *event)
+{
+	c_socket = nullptr;
+	c_setting = setting;
+	c_socket = new SelectSocket;
+	c_socket->Open(c_setting);
+	IsAccepting = false;
+	c_event = event;
+}
+
+void ListenThread::OnExecute()
+{
+	while (!IsTerminate())
+	{
+		IsAccepting = true;
+		try
+		{
+			SelectSocket *clientSocket = c_socket->Accept(); //监听客户端的连接
+			IsAccepting = false;
+			if (clientSocket != nullptr)
+			{
+				c_Section.Locked();
+				try
+				{
+					//接收或发送，数据的线程
+					Thread *child = new HandleThread(clientSocket,c_event);
+					child->RegisterNotifition(this);
+					child->Resume();
+					childThreads.push_back(child);
+				}
+				catch (...)
+				{}
+				c_Section.UnLocked();
+			}
+		}
+		catch (const std::exception&e)
+		{
+			LOGEXCEPTION(e);
+		}
+		catch (...)
+		{
+			LOGERROR(L"ListenThread::OnExecute()", L"发生未知的异常错误");
+		}
+	}
+}
+
+void ListenThread::OnTerminate(Thread *thread)
+{
+	c_Section.Locked();
+	try
+	{
+		for (int i = 0; i < childThreads.size(); i++)
+		{
+			if (childThreads[i] == thread)
+			{
+				childThreads.erase(childThreads.begin() + i);
+				break;
+			}
+		}
+	}
+	catch (...)
+	{}
+	c_Section.UnLocked();
+}
+
+ListenThread::~ListenThread()
+{
+	c_Section.Locked();
+	try 
+	{
+		for (int i = 0; i < (int)childThreads.size(); i++)
+		{
+			childThreads[i]->RegisterNotifition(nullptr); //不要再通知了，回调对方了
+			childThreads[i]->Terminate();
+		}
+	}
+	catch (...) {}
+	c_Section.UnLocked();
+	Terminate();
+	if (IsAccepting == true)
+		Suspend();
+	if (c_socket != nullptr)
+		delete c_socket;
+	c_socket = nullptr;
 }
 
 
@@ -75,7 +171,6 @@ AcceptThread::~AcceptThread()
 DSPServer::DSPServer(const SockSetting& s)
 {
 	c_sockSetting = s;
-	c_socket = nullptr;
 	c_running = false;
 	c_thread = nullptr;
 }
@@ -91,11 +186,8 @@ bool DSPServer::Start()
 		return true;
 	try
 	{
-		//启动socket
-		c_socket = new SelectSocket;
-		c_socket->Open(c_sockSetting);
 		//启动监听线程
-		c_thread = new AcceptThread(c_socket);
+		c_thread = new ListenThread(c_sockSetting,c_event);
 		c_thread->RegisterNotifition(this);
 		c_thread->Resume();
 		c_running = true;
@@ -118,12 +210,9 @@ void DSPServer::Close()
 	if (c_thread != nullptr)
 	{
 		c_thread->Terminate();
+		if (!c_thread->IsActive())
+			delete c_thread; //避免重复释放对象
 		c_thread = nullptr;
-	}
-	if (c_socket != nullptr)
-	{
-		delete c_socket;
-		c_socket = nullptr;
 	}
 	c_running = false;
 }
@@ -131,6 +220,11 @@ void DSPServer::Close()
 bool DSPServer::Running()
 {
 	return c_running;
+}
+
+void DSPServer::RegisterNetServerNotifyEvent(LNetServerNotifyEvent *event)
+{
+	c_event = event;
 }
 
 void DSPServer::OnTerminate(Thread *thread)
--- include/DSPServer.h
+++ include/DSPServer.h
@@ -9,6 +9,21 @@ desc:数据存储协议 Date Store Pro
 #include "..\include\BaseSocket.h"
 #include "..\include\Logger.h"
 #include "..\include\Thread.h"
+
+
+
+class LNetServerNotifyEvent
+{
+public:
+	//!当客户端与服务器建立连接时发出
+	virtual void OnConnect(SelectSocket *Handle) {};
+	//!当客户端与服务器进行数据交换时发生
+	virtual void OnExecute(SelectSocket *Handle) = 0;
+	//!当客户端与服务器断开连接时发生
+	virtual void OnDisconnect(SelectSocket *Handle) {};
+};
+
+
 class AFX_EXT_CLASS DSPServer : public ThreadNotifition
 {
 public:
@@ -18,12 +33,16 @@ public:
 	bool Start();//启动服务
 	void Close();
 	bool Running();
+
+	void RegisterNetServerNotifyEvent(LNetServerNotifyEvent *event);
 protected:
 	void OnTerminate(Thread *thread);
 private:
 	SockSetting c_sockSetting;
-	SelectSocket *c_socket;
 	bool c_running;
 	Thread *c_thread;
+	LNetServerNotifyEvent *c_event;
 };
+
+
 
